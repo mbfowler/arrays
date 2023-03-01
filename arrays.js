cdc1eb9468d24ae13f8d2dc1936637322af7d3cc
Matt Fowler <mbfowler@gmail.com>
2023-03-01 17:09:04 -0500

    Use a variety of array methods and explain what is happening

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var stringArray = ["totally", "absoultely", "agreed", "yep"];
 var numberArray = [2, 5, 7, 12];
 var boolArray = [true, true, false, false];
+
+stringArray.pop();
+// This will destructively return the final element of the array. So this returns "yep"
+
+console.log(numberArray.slice(1, 3));
+// This will non-destructively return an array including the elements at index 1 and up to but not including element 3.  So this returns [5, 7].
+
+boolArray.unshift(false);
+// This will add another element false to the front of (index 0) our boolArray
